c38be77fee5d50665f1dd99ddbb692336b686f37
Felix <fanrong19910103@proton.me>
2026-04-14 10:38:05 +0800

    meta_knowledge_service

--- app/services/meta_knowledge_service.py
+++ app/services/meta_knowledge_service.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
 from app.repositories.mysql.meta.meta_mysql_repository import MetaMySQLRepository
+from app.models.table_info import TableInfoMySQL
+from app.models.column_info import ColumnInfoMySQL
+from omegaconf import OmegaConf
+from app.conf.meta_config import MetaConfig
 
 
 class MetaKnowledgeService:
@@ -13,4 +17,14 @@ class MetaKnowledgeService:
         self.dw_mysql_repository = dw_mysql_repository
 
     async def build(self, config_path: Path):
-        pass
+        context = OmegaConf.load(config_path)
+        schema = OmegaConf.structured(MetaConfig)
+        meta_config: MetaConfig = OmegaConf.to_object(OmegaConf.merge(schema, context))
+        if meta_config.tables:
+            table_info:list[TableInfoMySQL] = []
+            column_info:list[ColumnInfoMySQL] = []
+            for table in meta_config.tables:
+                pass
+
+                for column in table.columns:
+                    pass
